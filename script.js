a22b126c4cc9da6096c09fddca40b5117f2f6522
Kanami shoji <lislilylis1159@gmail.com>
2021-05-01 23:43:32 +0900

    素材dl,carousel,font

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 
 $(document).ready(function () {
-  
+
   $('.counter').counterUp({
     delay: 10,
     time: 1000
   });
 
 });
+
+/* $(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    margin: 10,
+    loop: true,
+  });
+});
+ */
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    600: {
+      items: 3,
+      nav: false
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false
+    }
+  }
+})
