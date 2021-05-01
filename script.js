a4ca8ca285188277ba299baa21903f0375ab01c6
Kanami shoji <lislilylis1159@gmail.com>
2021-05-02 02:12:18 +0900

    ourstory,specialpackge

--- script.js
+++ script.js
@@ -6,33 +6,38 @@ $(document).ready(function () {
     time: 1000
   });
 
+  /* $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 3
+      }
+    }
+  }) */
+
+
 });
 
-/* $(function () {
+
+$(document).ready(function () {
   $('.owl-carousel').owlCarousel({
-    items: 4,
-    margin: 10,
+    center: true,
+    items: 1,
     loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      600: {
+        items: 1
+      }
+    }
   });
 });
- */
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: true
-    },
-    600: {
-      items: 3,
-      nav: false
-    },
-    1000: {
-      items: 5,
-      nav: true,
-      loop: false
-    }
-  }
-})
